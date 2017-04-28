97351833836db18618655cec42fb3e6711aea16e
heyjulieh <heyjulieh@gmail.com>
2017-04-28 15:55:29 -0700

    set up app and index controller files

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,26 @@
 console.log('this is working!')
+
+angular
+  .module('readingrainbow', ['ngRoute'])
+  .config(config);
+
+config.$inject = ['$routeProvider', '$locationProvider'];
+
+function config ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: '/templates/books/bookindex.html',
+      controllerAs: 'booksIndexCtrl',
+      controller: 'BooksIndexController'
+    })
+    .when('/books/:id', {
+      templateUrl: '/templates/books/bookshow.html',
+      controllerAs: 'booksShowCtrl',
+      controller: 'BooksShowController'
+    })
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+  });
+}
--- public/scripts/controllers/BooksIndexController.js
+++ public/scripts/controllers/BooksIndexController.js
@@ -1,1 +1,63 @@
 console.log('this is working!')
+
+angular
+  .module('readingrainbow')
+  .controller('BooksIndexController', BooksIndexController);
+
+BooksIndexController.$inject = ['$http'];
+
+function BooksIndexController ($http) {
+  var vm = this;
+  vm.newBook = {};
+  vm.newBook = {
+    title: 'Work Rules!: Insights from Inside Google That Will Transform How You Live and Lead',
+    author: 'Lazlo Bock',
+    releaseDate: 'April 7, 2015'
+    image: 'https://www.workrules.net/img/work-rules.png'
+  };
+
+  $http({
+    method: 'GET',
+    url: 'https://super-crud.herokuapp.com/books'
+  }).then(function successCallback(response) {
+    vm.books = response.data.books;
+  }, function errorCallback(response) {
+    console.log('There was an error getting the books', response);
+  });
+
+  vm.createBook = function () {
+    $http({
+      method: 'POST',
+      url: 'https://super-crud.herokuapp.com/books',
+      data: vm.newBook
+    }).then(function successCallback(response) {
+      vm.books.push(response.data.books);
+    }, function errorCallback(response) {
+      console.log('There was an error posting the book', response);
+    });
+  }
+
+  vm.editBook = function (book) {
+    $http({
+      method: 'PUT',
+      url: 'https://super-crud.herokuapp.com/books/' + book._id,
+      data: book
+    }).then(function successCallback(json) {
+      // don't need to do anything!
+    }, function errorCallback(response) {
+      console.log('There was an error editing the book', response);
+    });
+  }
+
+  vm.deleteBook = function (book) {
+    $http({
+      method: 'DELETE',
+      url: 'https://super-crud.herokuapp.com/books'+ '/' + book._id
+    }).then(function successCallback(json) {
+      var index = vm.books.indexOf(book);
+      vm.books.splice(index,1)
+    }, function errorCallback(response) {
+      console.log('There was an error deleting the book', response);
+    });
+  }
+}
